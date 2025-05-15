39e1976fb2c240707fe74ae5e67a5437e79886e8
uaich123 <98252856+uaich123@users.noreply.github.com>
2025-05-14 23:42:56 -0600

    Update home.py and fix some text

--- app/home.py
+++ app/home.py
@@ -4,7 +4,7 @@ st.title("Tuition Cost Predictor for International Students")
 
 st.write("""Are you worried about college expenses, especially as an international student?
 
-Don't stress — I've got you covered.
+Don't stress, because I've got you covered.
 
 This free tool helps you predict tuition costs for 622 universities around the world, based on your program details, living costs, and more.
 """)
