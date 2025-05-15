df391d684328465b2522916dc4959dd0b199b43f
uaich123 <98252856+uaich123@users.noreply.github.com>
2025-05-14 23:56:50 -0600

    Make a minor correction for wording.

--- app/about.py
+++ app/about.py
@@ -5,5 +5,5 @@ st.write("Urjit Aich is a dedicated Software Engineer and a Machine Learning Ent
 "is currently pursuing a B.S. in Computer Science at Purdue University. He loves to develop and " \
 "take part in projects that bring impact and serve the community. In his free time, he enjoys " \
 "going on walks, playing video games, coding, and watching movies.")
-st.write("You can find him on [GitHub](https://github.com/uaich123) and [LinkedIn](https://linkedin.com/in/urjit-aich)")
-st.write("If you want to know more about him, you can check out his [website](https://uaich123.github.io)")
+st.write("You can find him on [GitHub](https://github.com/uaich123) and [LinkedIn](https://linkedin.com/in/urjit-aich).")
+st.write("If you want to know more about him, you can check out his [website](https://uaich123.github.io).")
